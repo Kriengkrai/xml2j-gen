94e2f7114a8e9c1dc66638d13ba016975009a2ab
Lolke Dijkstra <lolkedijkstra@gmail.com>
2018-03-28 19:58:35 +0200

    empty test

---
+++ src/test/java/com/xml2j/Xml2jConfigurationTest.java
@@ -0,0 +1,14 @@
+package com.xml2j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Xml2jConfigurationTest {
+
+    @Test
+    public void testConfiguration() {
+
+    }
+
+}
---
+++ src/test/java/com/xml2j/Xml2jGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.xml2j;
+
+import org.junit.*;
+
+public class Xml2jGeneratorTest {
+
+    @Test
+    public void testCommandLine() {
+
+    }
+
+}
