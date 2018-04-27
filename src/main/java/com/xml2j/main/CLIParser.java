ba973cd48ceb58dba38b5e7db3922b8d97c1c53f
Lolke Dijkstra <lolkedijkstra@gmail.com>
2018-04-27 12:15:52 +0200

    2.6.0 now uses Apache CLI

--- src/main/java/com/xml2j/main/CLIParser.java
+++ src/main/java/com/xml2j/main/CLIParser.java
@@ -1,71 +1,135 @@
 package com.xml2j.main;
 
+import org.apache.commons.cli.*;
 import org.slf4j.LoggerFactory;
+import static java.lang.String.format;
 
 /**
  * Command line options.
  **/
 
-class CommandLine {
-    private static Notification logger = new Notification(LoggerFactory.getLogger(CommandLine.class));
+class CLIParser {
+    private static Notification logger = new Notification(LoggerFactory.getLogger(CLIParser.class));
 
-    // @formatter:off
-    /** command line parameters */
-    static final String usage = "Error: Must provide configuration file. Option: -c"
-            + "\n\t-? prints this message"
-            + "\n\t-l print license details"
-            + "\n\t-w working directory either absolute or relative to xml2j.jar's directory"
-            + "\n\t-h[customheader] insert custom header"
-            + "\n\t-s[UID] support for serialization with optional UID"
-            + "\n\t-p generate printing methods"
-            + "\n\t-v verbose"
-            + "\n\t-i output intermediate results"
-            + "\n\t-r remove old code before generating code"
-            + "\n\t-version prints version";
-    // @formatter:on
+    private Options options = new Options();
+    private CommandLineParser parser = new DefaultParser();
+    private HelpFormatter helpFormatter = new HelpFormatter();
+    private CommandLine commandLine = null;
 
-    /** print command line usage */
-    static void usage() {
-        logger.message(usage);
+    private static final String APPLICATION = "XML2J-GEN";
+
+    void printHelp() {
+        helpFormatter.printHelp(APPLICATION, options);
     }
 
-    static String headerFile = null;
+    CLIParser() {
+        options.addOption("p", "print", false, "generate printing methods (defaults to none)")
+                .addOption("l", "license", false, "print license details")
+                .addOption("?", "help", false, "prints help")
+                .addOption("w", "workingdirectory", true, "working directory either absolute or relative to xml2j.jar's directory")
+                .addOption("h", "header", true, "insert custom header")
+                .addOption("s", "serialVersionUID", true, "serialVersionUID with (long) ID (defaults to 1L)")
+                .addOption("v", "verbose", false, "verbose")
+                .addOption("i", "intermediate", false, "output intermediate results")
+                .addOption("r", "remove", false, "remove old code before generating code")
+                .addOption("version", false, "prints version")
+                .addOption("pom", "pom",false, "generate POM files (defaults to none)")
+                .addOption("a", "author", false, format("author (defaults to %s)", APPLICATION))
+                .addOption("c", "configuration", true, "(mandatory) configuration file used by generator");
+    }
 
-    static void parse(final String[] args) {
-        for (String arg : args) {
-            if (arg.startsWith("-a") && arg.length() > 2) {
-                Options.author = arg.substring(2);
-            } else if (arg.startsWith("-h")) {
-                headerFile = arg.substring(2);
-            } else if (arg.startsWith("-c")) {
-                Options.configFileName = arg.substring(2);
-            } else if (arg.startsWith("-s")) {
-                try {
-                    Options.UID = Long.parseLong(arg.substring(2));
-                } catch (Exception e) {
-                    logger.message("Warning: option -s ignored (not an integral number)");
-                }
-            } else if (arg.equals("-p")) {
-                Options.printMethods = true;
-            } else if (arg.startsWith("-w")) {
-                Options.workingDirectory = arg.substring(2);
-            } else if (arg.equals("-v")) {
-                Options.verbose = true;
-            } else if (arg.equals("-l")) {
-                Options.printLicense = true;
-            } else if (arg.equals("-i")) {
-                Options.intermediate = true;
-            } else if (arg.equals("-r")) {
-                Options.removeOldCode = true;
-            } else if (arg.equals("-version")) {
-                Options.printVersion = true;
-            } else if (arg.equals("-pom")) {
-                Options.pom = true;
+    private boolean isOptionSet(String opt) {
+        if (commandLine == null)
+            throw new NullPointerException("Commandline has not been parsed. Must use parse first");
+
+        return commandLine.hasOption(opt);
+    }
+
+    private String getValue(String name) {
+        return isOptionSet(name) ? commandLine.getOptionValue(name) : null;
+    }
+
+    boolean doPrintVersion() {
+        return isOptionSet("version");
+    }
+    boolean doPrintLicense() {
+        return isOptionSet("l");
+    }
+
+    boolean doPrintHelp() {
+        return isOptionSet("?");
+    }
+
+    String getAuthor() {
+        String author = getValue("a");
+        return author == null ? APPLICATION : author;
+    }
+
+    String getConfigfileName() {
+        return getValue("c");
+    }
+
+    String getHeader() {
+        return getValue("h");
+    }
+
+    String getWorkingDirectory() {
+        return getValue("w");
+    }
+
+    private boolean serialVersionNaN = false;
+    private long serialDefaultVersionUID = 1L;
+    private long serialVersionUID = 0L;         // if 0L -> not set
+
+    long getSerialize() {
+        if(serialVersionUID != 0L)
+            return serialVersionUID;
+
+        if(serialVersionNaN)
+            return serialDefaultVersionUID;
+
+        if (hasSerialize()) {
+            try {
+                serialVersionUID = Long.parseLong(getValue("s"));
+                return serialVersionUID;
+            } catch (NumberFormatException e) {
+                logger.warn(format("serialVersionUID NaN: Provided \"%s\", defaulting to 1L.", getValue("s")));
+                serialVersionNaN = true;
             }
-        }
-
-        if (Options.verbose) {
-            Options.print();
-        }
+        };
+        return serialDefaultVersionUID;
     }
+
+    boolean hasConfig() {
+        return isOptionSet("c");
+    }
+    boolean hasIntermediate() {
+        return isOptionSet("i");
+    }
+    boolean hasHeader() {
+        return isOptionSet("h");
+    }
+    boolean hasPom() {
+        return isOptionSet("pom");
+    }
+    boolean hasPrint() {
+        return isOptionSet("p");
+    }
+    boolean hasRemove() {
+        return isOptionSet("r");
+    }
+    boolean hasSerialize() {
+        return isOptionSet("s");
+    }
+    boolean hasVerbose() {
+        return isOptionSet("v");
+    }
+    boolean hasWorkingDirectory() {
+        return isOptionSet("w");
+    }
+
+    void parse(String[] args) throws ParseException {
+        commandLine = parser.parse(options, args);
+    }
+
 }
